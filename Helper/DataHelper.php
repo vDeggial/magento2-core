a097c6ba06723d121fc65652251bcfe298b74049
Oleg K <voknevark@gmail.com>
2019-12-09 16:18:49 -0800

    Update DataHelper.php

--- Helper/DataHelper.php
+++ Helper/DataHelper.php
@@ -5,12 +5,6 @@ use Magento\Framework\App\Helper\Context;
 
 class DataHelper extends BaseHelper
 {
-    public function __construct(
-        \Magento\Framework\App\Helper\Context $context
-    ) {
-        parent::__construct($context);
-    }
-    
     public function getConfigFlag($path, $scopeCode = null)
     {
         return $this->scopeConfig->isSetFlag($path,\Magento\Store\Model\ScopeInterface::SCOPE_STORE, $scopeCode);
