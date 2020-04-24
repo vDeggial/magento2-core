257f83a07850ad9c27cfb385d68c8e51aa9b8841
Oleg K <voknevark@gmail.com>
2020-04-24 11:39:26 -0700

    misc

--- app/code/Hapex/Core/Helper/CustomerHelper.php
+++ app/code/Hapex/Core/Helper/CustomerHelper.php
@@ -13,13 +13,40 @@ class CustomerHelper extends BaseHelper
         $this->session = $this->generateClassObject('Magento\Customer\Model\SessionFactory')->create();
     }
 
+    public function getCustomerById($customerId = 0)
+    {
+        $factory = $this->generateClassObject("Magento\Customer\Model\CustomerFactory")->create();
+        return $factory->load($customerId);
+    }
+
+    public function getCustomAttributeValue($customer = null, $attribute = null)
+    {
+        $value = null;
+        try {
+            $repository = $this->generateClassObject("Magento\Customer\Api\CustomerRepositoryInterface");
+            $customer = $repository->getById($customer->getId());
+            $attribute = $customer->getCustomAttribute($attribute);
+            $value = $attribute !== null ? $attribute->getValue() : false;
+        } catch (\Exception $e) {
+            $value = null;
+        } finally {
+            return $value;
+        }
+    }
+
     public function getLoggedInGroup()
     {
-        return $this->isLoggedIn() ? $this->session->getCustomer()->getGroupId() : 0;
+        $customer = $this->getLoggedInCustomer();
+        return $customer != null ? $customer->getGroupId() : 0;
     }
 
     public function isLoggedIn()
     {
         return $this->session && $this->session->isLoggedIn();
+    }
+
+    public function getLoggedInCustomer()
+    {
+        return $this->isLoggedIn() ? $this->session->getCustomer() : null;
     }
 }
