9744879fe12f4b044bb924ecacbdea75b9c6c3a4
Oleg K <voknevark@gmail.com>
2020-05-31 10:34:36 -0700

    Update Helper/SalesRuleHelper.php

--- app/code/Hapex/Core/Helper/SalesRuleHelper.php
+++ app/code/Hapex/Core/Helper/SalesRuleHelper.php
@@ -6,60 +6,100 @@ use Magento\Framework\App\Helper\Context;
 
 class SalesRuleHelper extends BaseHelper
 {
-    protected $ruleRepository;
+    protected $salesRuleTableName = null;
 
     public function __construct(Context $context, ObjectManagerInterface $objectManager)
     {
         parent::__construct($context, $objectManager);
-        $this->ruleRepository = $this->generateClassObject("Magento\SalesRule\Model\RuleRepository");
+        $this->salesRuleTableName = "salesrule";
     }
 
     protected function getSalesRules($salesRuleId = 0)
     {
         $rule = null;
-        switch (is_array($salesRuleId)) {
-          case true:
-              $rule = [];
-              foreach ($salesRuleId as $ruleId) {
-                  $rule[] = $this->ruleRepository->getById($ruleId);
-              }
-              break;
+        try {
+            switch (is_array($salesRuleId)) {
+                case true:
+                    $rule = [];
+                    foreach ($salesRuleId as $ruleId) {
+                        $rule[] = $this->getSalesRule($ruleId);
+                    }
+                break;
 
-          default:
-              $rule = $salesRuleId != 0 ? $this->ruleRepository->getById($salesRuleId) : null;
-              break;
-      }
-        return $rule;
+                default:
+                    $rule = $this->getSalesRule($salesRuleId);
+                break;
+            }
+        } catch (\Exception $e) {
+            $this->errorLog($e->getMessage());
+            $rule = null;
+        } finally {
+            return $rule;
+        }
+    }
+
+    private function getSalesRule($salesRuleId = 0)
+    {
+        $rule = null;
+        try {
+            $tableSalesRule = $this->getSqlTableName($this->salesRuleTableName);
+            $sql = "SELECT rule_id, from_date, to_date, is_active FROM $tableSalesRule WHERE rule_id = $salesRuleId";
+            $result = $this->sqlQueryFetchRow($sql);
+            $rule = [];
+            $rule["id"] = (int)$result["rule_id"];
+            $rule["active"] = (bool)$result["is_active"];
+            $rule["from_date"] = $result["from_date"];
+            $rule["to_date"] = $result["to_date"];
+        } catch (\Exception $e) {
+            $this->errorLog($e->getMessage());
+            $rule = null;
+        } finally {
+            return $rule;
+        }
     }
 
     protected function getSalesRuleStatus($rule)
     {
-        switch ($rule !== null) {
-          case true:
-              return $rule->getIsActive();
-      }
+        $isActive = false;
+        try {
+            $isActive = $rule["active"];
+        } catch (\Exception $e) {
+            $this->errorLog($e->getMessage());
+            $isActive = false;
+        } finally {
+            return $isActive;
+        }
     }
 
     protected function isValidRuleDates($rule)
     {
-        switch ($rule !== null) {
-          case true:
-              $fromDate = $rule->getFromDate();
-              $toDate = $rule->getToDate();
-              return $this->isCurrentDateWithinRange($fromDate, $toDate);
-
-          default:
-              return false;
-      }
+        $isValid = false;
+        try {
+            $fromDate = $rule["from_date"];
+            $toDate = $rule["to_date"];
+            $isValid = $this->isCurrentDateWithinRange($fromDate, $toDate);
+        } catch (\Exception $e) {
+            $this->errorLog($e->getMessage());
+            $isValid = false;
+        } finally {
+            return $isValid;
+        }
     }
 
     protected function setSalesRuleStatus($rule, $status = 0)
     {
-        switch ($rule !== null) {
-          case true:
-              $rule->setIsActive($status);
-              $this->ruleRepository->save($rule);
-              break;
-      }
+        $isSet = false;
+        try {
+            $ruleId = $rule["id"];
+            $tableSalesRule = $this->getSqlTableName($this->salesRuleTableName);
+            $sql = "UPDATE $tableSalesRule SET is_active = $status where rule_id = $ruleId";
+            $result = $this->sqlQuery($sql);
+            $isSet = true;
+        } catch (\Exception $e) {
+            $this->errorLog($e->getMessage());
+            $isSet = false;
+        } finally {
+            return $isSet;
+        }
     }
 }
