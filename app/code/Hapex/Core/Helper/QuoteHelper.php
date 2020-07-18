9688991e4f5f80f5955f26b1df3374a31d931fab
Oleg K <voknevark@gmail.com>
2020-07-18 09:41:14 -0700

    Update Helper/QuoteHelper.php

--- app/code/Hapex/Core/Helper/QuoteHelper.php
+++ app/code/Hapex/Core/Helper/QuoteHelper.php
@@ -1,13 +1,51 @@
 <?php
 namespace Hapex\Core\Helper;
+
 use Magento\Framework\ObjectManagerInterface;
 use Magento\Framework\App\Helper\Context;
 
 class QuoteHelper extends BaseHelper
 {
-	public function __construct(Context $context, ObjectManagerInterface $objectManager)
-	{
+    protected $tableQuote;
+    protected $tableQuoteItem;
+    public function __construct(Context $context, ObjectManagerInterface $objectManager)
+    {
+        parent::__construct($context, $objectManager);
+        $this->tableQuote = $this->getSqlTableName('quote');
+        $this->tableQuoteItem = $this->getSqlTableName('quote_item');
+    }
 
-		parent::__construct($context, $objectManager);
-	}
+    public function getQuote($quoteId)
+    {
+        return $this->getQuoteById($quoteId);
+    }
+
+    public function quoteExists($quoteId)
+    {
+        $exists = false;
+        try {
+            $sql = "SELECT * FROM " . $this->tableQuote . " quote where quote.entity_id = $productId";
+            $result = $this->sqlQueryFetchOne($sql);
+            $exists = $result && !empty($result);
+        } catch (\Exception $e) {
+            $this->errorLog(__METHOD__ . " | " . $e->getMessage());
+            $exists = false;
+        } finally {
+            return $exists;
+        }
+    }
+
+    private function getQuoteById($quoteId)
+    {
+        $quote = null;
+        try {
+            $quoteFactory = $this->generateClassObject("Magento\Quote\Model\QuoteFactory");
+            $quote = $this->quoteExists($quoteId) ? $quoteFactory->create()->load($quoteId) : null;
+        } catch (\Exception $e) {
+            $this->errorLog(__METHOD__ . " | " . $e->getMessage());
+            $quote = null;
+        } finally {
+            return $quote;
+        }
+    }
 }
