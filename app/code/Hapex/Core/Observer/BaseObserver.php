f7d6658f48fa68bb77584a209a682cda67da19ea
Oleg K <voknevark@gmail.com>
2020-08-18 23:01:36 -0700

    Add BaseObserver.php

---
+++ app/code/Hapex/Core/Observer/BaseObserver.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Hapex\Core\Observer;
+
+use Hapex\Core\Helper\LogHelper;
+use Hapex\Core\Helper\DataHelper;
+use Magento\Framework\DataObject;
+use Magento\Framework\Event\Observer;
+use Magento\Framework\Event\ObserverInterface;
+use Magento\Framework\Message\ManagerInterface;
+
+class BaseObserver implements ObserverInterface
+{
+
+    protected $helperLog;
+    protected $helperData;
+    protected $messageManager;
+    protected $event;
+
+    public function __construct(
+        DataHelper $helperData,
+        LogHelper $helperLog,
+        ManagerInterface $messageManager
+    ) {
+        $this->helperData = $helperData;
+        $this->helperLog = $helperLog;
+        $this->messageManager = $messageManager;
+        $this->event = $this->helperData->generateClassObject(DataObject::class);
+    }
+
+    public function execute(Observer $observer)
+    {
+        $this->event = $observer->getEvent();
+    }
+}
