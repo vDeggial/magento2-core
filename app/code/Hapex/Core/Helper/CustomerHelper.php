505132a3d95bfc2dea4cecc4570b1f7138b023ed
Oleg K <voknevark@gmail.com>
2020-09-02 10:05:30 -0700

    Update Helper\CustomerHelper.php

--- app/code/Hapex/Core/Helper/CustomerHelper.php
+++ app/code/Hapex/Core/Helper/CustomerHelper.php
@@ -12,6 +12,7 @@ class CustomerHelper extends BaseHelper
     protected $session;
     protected $customerFactory;
     protected $helperEav;
+    protected $tableCustomer;
     public function __construct(
         Context $context,
         ObjectManagerInterface $objectManager,
@@ -23,6 +24,7 @@ class CustomerHelper extends BaseHelper
         $this->helperEav = $helperEav;
         $this->session = $sessionFactory->create();
         $this->customerFactory = $customerFactory->create();
+        $this->tableCustomer = $this->helperDb->getSqlTableName('customer_entity');
     }
 
     public function getCustomer($customerId = 0)
@@ -30,10 +32,98 @@ class CustomerHelper extends BaseHelper
         return $this->getCustomerById($customerId);
     }
 
-    public function getCustomerByEmail($email = null)
+    public function customerExists($customerId = 0)
     {
-        $websiteId = $this->generateClassObject(\Magento\Store\Model\StoreManagerInterface::class)->getStore()->getWebsiteId();
-        return $this->customerFactory->setWebsiteId($websiteId)->loadByEmail($email);
+        $exists = false;
+        try {
+            $sql = "SELECT * FROM " . $this->tableCustomer . " customer where customer.entity_id = $customerId";
+            $result = $this->helperDb->sqlQueryFetchOne($sql);
+            $exists = $result && !empty($result);
+        } catch (\Exception $e) {
+            $this->helperLog->errorLog(__METHOD__, $e->getMessage());
+            $exists = false;
+        } finally {
+            return $exists;
+        }
+    }
+
+    public function getCustomerEmail($customerId)
+    {
+        $customerEmail = null;
+        try {
+            $customerEmail = $this->helperEav->getCustomerEntityFieldValue($customerId, "email");
+        } catch (\Exception $e) {
+            $this->helperLog->errorLog(__METHOD__, $e->getMessage());
+            $customerEmail = null;
+        } finally {
+            return $customerEmail;
+        }
+    }
+
+    public function getCustomerGroup($customerId)
+    {
+        $customerGroup = 0;
+        try {
+            $customerGroup = (int) $this->helperEav->getCustomerEntityFieldValue($customerId, "group_id");
+        } catch (\Exception $e) {
+            $this->helperLog->errorLog(__METHOD__, $e->getMessage());
+            $customerGroup = 0;
+        } finally {
+            return $customerGroup;
+        }
+    }
+
+    public function getCustomerGender($customerId)
+    {
+        $customerGender = 0;
+        try {
+            $customerGender = (int) $this->helperEav->getCustomerEntityFieldValue($customerId, "gender");
+        } catch (\Exception $e) {
+            $this->helperLog->errorLog(__METHOD__, $e->getMessage());
+            $customerGender = 0;
+        } finally {
+            return $customerGender;
+        }
+    }
+
+    public function getCustomerFirstName($customerId)
+    {
+        $customerName = null;
+        try {
+            $customerName = $this->helperEav->getCustomerEntityFieldValue($customerId, "firstname");
+        } catch (\Exception $e) {
+            $this->helperLog->errorLog(__METHOD__, $e->getMessage());
+            $customerName = null;
+        } finally {
+            return $customerName;
+        }
+    }
+
+    public function getCustomerLastName($customerId)
+    {
+        $customerName = null;
+        try {
+            $customerName = $this->helperEav->getCustomerEntityFieldValue($customerId, "lastname");
+        } catch (\Exception $e) {
+            $this->helperLog->errorLog(__METHOD__, $e->getMessage());
+            $customerName = null;
+        } finally {
+            return $customerName;
+        }
+    }
+
+    public function getCustomerIdByEmail($customerEmail)
+    {
+        $customerId = 0;
+        try {
+            $sql = "SELECT entity_id FROM " . $this->tableCustomer . " WHERE email like '$customerEmail'";
+            $customerId = (int) $this->helperDb->sqlQueryFetchOne($sql);
+        } catch (\Exception $e) {
+            $this->helperLog->errorLog(__METHOD__, $e->getMessage());
+            $customerId = 0;
+        } finally {
+            return $customerId;
+        }
     }
 
     public function getAttributeValue($customerId = null, $attribute = null)
@@ -64,18 +154,5 @@ class CustomerHelper extends BaseHelper
     private function getCustomerById($customerId = 0)
     {
         return $this->customerFactory->load($customerId);
-    }
-
-    private function getCustomerGroup($customerId)
-    {
-        $customerGroup = 0;
-        try {
-            $customerGroup = (int) $this->helperEav->getCustomerEntityFieldValue($customerId, "group_id");
-        } catch (\Exception $e) {
-            $this->helperLog->errorLog(__METHOD__, $e->getMessage());
-            $customerGroup = 0;
-        } finally {
-            return $customerGroup;
-        }
     }
 }
