e0502caa0f1f1be26293fd07fab5dff47707477c
Oleg K <voknevark@gmail.com>
2020-10-23 09:16:08 -0700

    Update Helper\LogHelper.php

--- app/code/Hapex/Core/Helper/LogHelper.php
+++ app/code/Hapex/Core/Helper/LogHelper.php
@@ -30,10 +30,9 @@ class LogHelper extends AbstractHelper
     private function writeLogEntry($filename = null, $message = null)
     {
         try {
-            $currentDate = $this->objectManager->get(DateHelper::class)->getCurrentDate()->format("d.m.Y H:i:s");
+            $currentDate = $this->objectManager->get(DateHelper::class)->getCurrentDate()->format("Y-m-d h:i:s A T");
             $filePath = $this->objectManager->get(FileHelper::class)->getRootPath() . "/var/log/$filename.log";
-            error_log("[$currentDate] " . $message . "\n", 3, $filePath);
-            return true;
+            return error_log("[$currentDate] " . $message . "\n", 3, $filePath);;
         } catch (\Exception $e) {
             return false;
         }
