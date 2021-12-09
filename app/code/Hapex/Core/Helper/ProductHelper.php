7a1290be3c6950237336006047506df8df2a66f7
deggial <oleg@deggial.pro>
2021-12-09 10:57:42 -0800

    misc

--- app/code/Hapex/Core/Helper/ProductHelper.php
+++ app/code/Hapex/Core/Helper/ProductHelper.php
@@ -338,7 +338,7 @@ class ProductHelper extends BaseHelper
     {
         $images = [];
         try {
-            $sql = "SELECT gal.value AS fileName FROM " . $this->tableGalleryToEntity . " ent LEFT JOIN " . $this->tableGalleryValue . " val ON ent.entity_id= val.entity_id LEFT JOIN " . $this->tableGallery . " gal ON val.value_id = gal.value_id WHERE ent.entity_id = $productId GROUP BY gal.value";
+            $sql = "SELECT gal.value AS fileName FROM " . $this->tableGalleryToEntity . " ent LEFT JOIN " . $this->tableGalleryValue . " val ON ent.entity_id= val.entity_id LEFT JOIN " . $this->tableGallery . " gal ON val.value_id = gal.value_id WHERE ent.entity_id = $productId GROUP BY gal.value ORDER BY val.position";
             $result = $this->helperDb->sqlQueryFetchAll($sql);
             array_walk($result, function ($entry) use (&$images) {
                 array_push($images, $entry["fileName"]);
