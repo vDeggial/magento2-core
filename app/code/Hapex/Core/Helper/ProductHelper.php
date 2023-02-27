f94c0b9d0c5f5bfae95e283495a5bc053808168f
deggial <oleg@deggial.pro>
2023-02-26 19:55:30 -0800

    misc

--- app/code/Hapex/Core/Helper/ProductHelper.php
+++ app/code/Hapex/Core/Helper/ProductHelper.php
@@ -75,11 +75,12 @@ class ProductHelper extends BaseHelper
     {
         $products = [];
         $tableStatus = $this->helperDb->getSqlTableName('catalog_product_entity_int');
+        $tableStockStatus = $this->helperDb->getSqlTableName('cataloginventory_stock_status');
         $tableAttribute = $this->helperDb->getSqlTableName("eav_attribute");
-        $sql = "SELECT entity_id FROM $tableStatus WHERE attribute_id = (SELECT attribute_id FROM $tableAttribute WHERE attribute_code LIKE 'status') AND $tableStatus.value = $status";
+        $sql = "SELECT product_id FROM $tableStockStatus where product_id in (SELECT entity_id FROM $tableStatus WHERE attribute_id = (SELECT attribute_id FROM $tableAttribute WHERE attribute_code LIKE 'status') AND $tableStatus.value = $status)";
         $result = $this->helperDb->sqlQueryFetchAll($sql);
         if ($result) {
-            foreach (array_column($result, "entity_id") as $entry) {
+            foreach (array_column($result, "product_id") as $entry) {
                 $sku = $this->getSku($entry);
                 $products[] = $sku;
             }
