a2a3efd0ded33b733ba6365012d6c5d99b2b6b69
deggial <oleg@deggial.pro>
2023-05-04 21:23:03 -0700

    misc

--- app/code/Hapex/Core/Cron/BaseCron.php
+++ app/code/Hapex/Core/Cron/BaseCron.php
@@ -9,10 +9,14 @@ class BaseCron
 {
     protected $helperData;
     protected $helperLog;
+    protected $maintenanceMode;
+    protected $isMaintenance;
 
     public function __construct(DataHelper $helperData, LogHelper $helperLog)
     {
         $this->helperData = $helperData;
         $this->helperLog = $helperLog;
+        $this->maintenanceMode = $this->helperData->generateClassObject(\Magento\Framework\App\MaintenanceMode::class);
+        $this->isMaintenance = isset($this->maintenanceMode) ? $this->maintenanceMode->isOn() : true;
     }
 }
