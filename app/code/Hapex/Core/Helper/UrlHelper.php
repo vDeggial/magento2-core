267d04653bd10a789d15860b0f903dea13ffc117
deggial <oleg@deggial.pro>
2023-10-02 13:57:47 -0700

    misc

--- app/code/Hapex/Core/Helper/UrlHelper.php
+++ app/code/Hapex/Core/Helper/UrlHelper.php
@@ -86,13 +86,13 @@ class UrlHelper extends AbstractHelper
                 CURLOPT_FOLLOWLOCATION => 1,
                 CURLOPT_USERAGENT => "Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.7) Gecko/20040803 Firefox/0.9.3",
                 CURLOPT_CONNECTTIMEOUT => 10,
-                CURLOPT_TIMEOUT => 60
+                CURLOPT_TIMEOUT => 20
             ];
             $this->curl->setOptions($options);
             $this->curl->get($url);
         } catch (\Throwable $e) {
             $this->helperLog->errorLog(__METHOD__, $this->getExceptionTrace($e));
-            //return $this->get($url);
+            return $this->get($url);
         } finally {
             return $this->curl;
         }
@@ -107,7 +107,7 @@ class UrlHelper extends AbstractHelper
                 CURLOPT_FOLLOWLOCATION => 1,
                 CURLOPT_USERAGENT => "Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.7) Gecko/20040803 Firefox/0.9.3",
                 CURLOPT_CONNECTTIMEOUT => 10,
-                CURLOPT_TIMEOUT => 60
+                CURLOPT_TIMEOUT => 20
             ];
             $headers = ["Content-Type" => $contentType];
             $this->curl->setOptions($options);
@@ -115,7 +115,7 @@ class UrlHelper extends AbstractHelper
             $this->curl->post($url, $data);
         } catch (\Throwable $e) {
             $this->helperLog->errorLog(__METHOD__, $this->getExceptionTrace($e));
-            //return $this->post($url, $data, $contentType);
+            return $this->post($url, $data, $contentType);
         } finally {
             return $this->curl;
         }
