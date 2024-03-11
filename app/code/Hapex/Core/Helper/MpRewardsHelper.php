49459ca5c90992d14504a4221dfa41e20c391e88
deggial <oleg@deggial.pro>
2024-03-11 10:57:03 -0700

    misc

--- app/code/Hapex/Core/Helper/MpRewardsHelper.php
+++ app/code/Hapex/Core/Helper/MpRewardsHelper.php
@@ -13,4 +13,116 @@ class MpRewardsHelper extends BaseHelper
         parent::__construct($context, $objectManager);
         $this->tableRewardTransactions = $this->helperDb->getSqlTableName("mageplaza_reward_transaction");
     }
+
+    public function getMpRewardTransactions($select = "*")
+    {
+        $result = null;
+        try {
+            $sql = "SELECT $select FROM " . $this->tableRewardTransactions;
+            $result = $this->helperDb->sqlQueryFetchAll($sql);
+        } catch (\Throwable $e) {
+            $this->helperLog->errorLog(__METHOD__, $this->helperLog->getExceptionTrace($e));
+            $result = null;
+        } finally {
+            return $result;
+        }
+    }
+
+    public function getMpRewardTransactionById($transactionId = 0, $select = "*")
+    {
+        $result = null;
+        try {
+            $sql = "SELECT $select FROM " . $this->tableRewardTransactions . " WHERE transaction_id = $transactionId";
+            $result = $this->helperDb->sqlQueryFetchRow($sql);
+        } catch (\Throwable $e) {
+            $this->helperLog->errorLog(__METHOD__, $this->helperLog->getExceptionTrace($e));
+            $result = null;
+        } finally {
+            return $result;
+        }
+    }
+
+    public function getMpRewardTransactionByCustomerId($customerId = 0, $select = "*")
+    {
+        $result = null;
+        try {
+            $sql = "SELECT $select FROM " . $this->tableRewardTransactions . " WHERE customer_id = $customerId";
+            $result = $this->helperDb->sqlQueryFetchAll($sql);
+        } catch (\Throwable $e) {
+            $this->helperLog->errorLog(__METHOD__, $this->helperLog->getExceptionTrace($e));
+            $result = null;
+        } finally {
+            return $result;
+        }
+    }
+
+    public function getMpRewardTransactionsByActionType($actionType = 0, $select = "*")
+    {
+        $result = null;
+        try {
+            $sql = "SELECT $select FROM " . $this->tableRewardTransactions . " WHERE action_type = $actionType";
+            $result = $this->helperDb->sqlQueryFetchAll($sql);
+        } catch (\Throwable $e) {
+            $this->helperLog->errorLog(__METHOD__, $this->helperLog->getExceptionTrace($e));
+            $result = null;
+        } finally {
+            return $result;
+        }
+    }
+
+    public function getMpRewardTransactionsByActionCode($actionCode = null, $select = "*")
+    {
+        $result = null;
+        try {
+            $sql = "SELECT $select FROM " . $this->tableRewardTransactions . " WHERE action_code LIKE '$actionCode'";
+            $result = $this->helperDb->sqlQueryFetchAll($sql);
+        } catch (\Throwable $e) {
+            $this->helperLog->errorLog(__METHOD__, $this->helperLog->getExceptionTrace($e));
+            $result = null;
+        } finally {
+            return $result;
+        }
+    }
+
+    public function getMpRewardTransactionsByStatus($status = 0, $select = "*")
+    {
+        $result = null;
+        try {
+            $sql = "SELECT $select FROM " . $this->tableRewardTransactions . " WHERE status = $status";
+            $result = $this->helperDb->sqlQueryFetchAll($sql);
+        } catch (\Throwable $e) {
+            $this->helperLog->errorLog(__METHOD__, $this->helperLog->getExceptionTrace($e));
+            $result = null;
+        } finally {
+            return $result;
+        }
+    }
+
+    public function getMpRewardTransactionsByDateCreated($dateCreated = null, $select = "*")
+    {
+        $result = null;
+        try {
+            $sql = "SELECT $select FROM " . $this->tableRewardTransactions . " WHERE created_at LIKE '%$dateCreated%'";
+            $result = $this->helperDb->sqlQueryFetchAll($sql);
+        } catch (\Throwable $e) {
+            $this->helperLog->errorLog(__METHOD__, $this->helperLog->getExceptionTrace($e));
+            $result = null;
+        } finally {
+            return $result;
+        }
+    }
+
+    public function getMpRewardTransactionsByExtraContent($extraContent = null, $select = "*")
+    {
+        $result = null;
+        try {
+            $sql = "SELECT $select FROM " . $this->tableRewardTransactions . " WHERE extra_content LIKE '%$extraContent%'";
+            $result = $this->helperDb->sqlQueryFetchAll($sql);
+        } catch (\Throwable $e) {
+            $this->helperLog->errorLog(__METHOD__, $this->helperLog->getExceptionTrace($e));
+            $result = null;
+        } finally {
+            return $result;
+        }
+    }
 }
