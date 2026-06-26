bcfaefdb119abe3893ecc34a6aa4474222314599
deggial <oleg@deggial.me>
2026-06-25 20:09:17 -0700

    misc

--- app/code/Hapex/Core/Helper/CustomerHelper.php
+++ app/code/Hapex/Core/Helper/CustomerHelper.php
@@ -62,7 +62,7 @@ class CustomerHelper extends BaseHelper
 
         //$where .= " AND subs.status = 'active'";
 
-        $where .= " AND subs.created_at = (SELECT MAX(created_at) FROM $tableStripeSubscriptions subs2 where subs2.stripe_customer_id = subs.stripe_customer_id AND subs2.status IN ('active', 'canceled', 'past_due', 'reactivated'))";
+        $where .= " AND subs.created_at = (SELECT MAX(created_at) FROM $tableStripeSubscriptions subs2 where subs2.stripe_customer_id = subs.stripe_customer_id AND subs2.status IN ('active', 'canceled', 'past_due', 'reactivated', 'incomplete'))";
 
         $sql = "SELECT $select FROM " . $this->tableCustomer . " customers JOIN $tableStripeSubscriptions subs ON customers.entity_id = subs.magento_customer_id WHERE $where";
 
